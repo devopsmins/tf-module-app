d6b4b2964ac5eb7a64e34fc8be860dc19e52a9cf
minhaj <minhajfathima1023@gmail.com>
2024-04-21 04:31:15 -0400

    TF code

--- data.tf
+++ data.tf
@@ -1,9 +1,3 @@
-#data "aws_ami" "ami" {
-  //most_recent = true
-  //name_regex  = "Centos-8-DevOps-Practice"
-  //owners      = ["973714476881"]
-#}
-
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "golden-ami"
